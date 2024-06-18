ab8b4a641f55d90631651b499f6c1961d7f21d1f
mikamikanomi <paulem234@gmail.com>
2024-06-18 16:37:52 +0900

    Dex씬 업데이트
    페이지에 따라 폴라로이드와 텍스트가 활성화되게 설정. 임시로 6페이지 정도 까지 제작

--- ProjectPlanter/Assets/Scripts/DexScript/BtnManager.cs
+++ ProjectPlanter/Assets/Scripts/DexScript/BtnManager.cs
@@ -14,8 +14,18 @@ public class BtnManager : MonoBehaviour
 
     public GameObject plantPr1;
     public GameObject plantPr2;
+    public GameObject plantPr3;
+    public GameObject plantPr4;
+    public GameObject plantPr5;
+    public GameObject plantPr6;
+
     public GameObject evText1;
     public GameObject evText2;
+    public GameObject evText3;
+    public GameObject evText4;
+    public GameObject evText5;
+    public GameObject evText6;
+
     public GameObject GameObject;
 
     private void Start()
@@ -54,8 +64,74 @@ public class BtnManager : MonoBehaviour
             this.btnNext.gameObject.SetActive(true);
         }
         this.btnPrev.gameObject.SetActive(true);
-    }
 
+        if (currentPage == 1)
+        {
+            plantPr1.gameObject.SetActive(true);
+            evText1.gameObject.SetActive(true);
+        }
+        else
+        {
+            plantPr1.gameObject.SetActive(false);
+            evText1.gameObject.SetActive(false);
+        }
+
+        if (currentPage == 2)
+        {
+            plantPr2.gameObject.SetActive(true);
+            evText2.gameObject.SetActive(true);
+        }
+        else
+        {
+            plantPr2.gameObject.SetActive(false);
+            evText2.gameObject.SetActive(false);
+        }
+
+        if (currentPage == 3)
+        {
+            plantPr3.gameObject.SetActive(true);
+            evText3.gameObject.SetActive(true);
+        }
+        else
+        {
+            plantPr3.gameObject.SetActive(false);
+            evText3.gameObject.SetActive(false);
+        }
+
+        if (currentPage == 4)
+        {
+            plantPr4.gameObject.SetActive(true);
+            evText4.gameObject.SetActive(true);
+        }
+        else
+        {
+            plantPr4.gameObject.SetActive(false);
+            evText4.gameObject.SetActive(false);
+        }
+
+        if (currentPage == 5)
+        {
+            plantPr5.gameObject.SetActive(true);
+            evText5.gameObject.SetActive(true);
+        }
+        else
+        {
+            plantPr5.gameObject.SetActive(false);
+            evText5.gameObject.SetActive(false);
+        }
+
+        if (currentPage == 6)
+        {
+            plantPr6.gameObject.SetActive(true);
+            evText6.gameObject.SetActive(true);
+        }
+        else
+        {
+            plantPr6.gameObject.SetActive(false);
+            evText6.gameObject.SetActive(false);
+        }
+
+    }
     public void PrevPage()
     {
         if (this.currentPage == 1)
@@ -76,5 +152,71 @@ public class BtnManager : MonoBehaviour
             this.btnPrev.gameObject.SetActive(true);
         }
         this.btnNext.gameObject.SetActive(true);
+
+        if (currentPage == 1)
+        {
+            plantPr1.gameObject.SetActive(true);
+            evText1.gameObject.SetActive(true);
+        }
+        else
+        {
+            plantPr1.gameObject.SetActive(false);
+            evText1.gameObject.SetActive(false);
+        }
+
+        if (currentPage == 2)
+        {
+            plantPr2.gameObject.SetActive(true);
+            evText2.gameObject.SetActive(true);
+        }
+        else
+        {
+            plantPr2.gameObject.SetActive(false);
+            evText2.gameObject.SetActive(false);
+        }
+
+        if (currentPage == 3)
+        {
+            plantPr3.gameObject.SetActive(true);
+            evText3.gameObject.SetActive(true);
+        }
+        else
+        {
+            plantPr3.gameObject.SetActive(false);
+            evText3.gameObject.SetActive(false);
+        }
+
+        if (currentPage == 4)
+        {
+            plantPr4.gameObject.SetActive(true);
+            evText4.gameObject.SetActive(true);
+        }
+        else
+        {
+            plantPr4.gameObject.SetActive(false);
+            evText4.gameObject.SetActive(false);
+        }
+
+        if (currentPage == 5)
+        {
+            plantPr5.gameObject.SetActive(true);
+            evText5.gameObject.SetActive(true);
+        }
+        else
+        {
+            plantPr5.gameObject.SetActive(false);
+            evText5.gameObject.SetActive(false);
+        }
+
+        if (currentPage == 6)
+        {
+            plantPr6.gameObject.SetActive(true);
+            evText6.gameObject.SetActive(true);
+        }
+        else
+        {
+            plantPr6.gameObject.SetActive(false);
+            evText6.gameObject.SetActive(false);
+        }
     }
 }
