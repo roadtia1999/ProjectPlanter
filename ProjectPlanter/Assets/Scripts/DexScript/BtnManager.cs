d4e996d78993867f82c272262bc8e3e70c43f2fd
roadtia1999 <132754149+roadtia1999@users.noreply.github.com>
2024-06-24 16:05:25 +0900

    Dex씬 수정 (ScriptableObject 호환)

--- ProjectPlanter/Assets/Scripts/DexScript/BtnManager.cs
+++ ProjectPlanter/Assets/Scripts/DexScript/BtnManager.cs
@@ -6,33 +6,23 @@ using UnityEngine.SceneManagement;
 
 public class BtnManager : MonoBehaviour
 {
-    private int currentPage = 1;
-    private int totalPage = 4;
+    public List<ItemData> database;
+    public GameObject dexSprite;
+    public Text dexTitleText;
+    public Text dexDescText;
+
+    private int currentPage = 0;
 
     public Button btnNext;
     public Button btnPrev;
 
-    public GameObject plantPr1;
-    public GameObject plantPr2;
-    public GameObject plantPr3;
-    public GameObject plantPr4;
-
-    public GameObject evText1;
-    public GameObject evText2;
-    public GameObject evText3;
-    public GameObject evText4;
-
-    public GameObject GameObject;
-
     private void Start()
     {
-        GameObject = GetComponent<GameObject>();
-
-        this.btnNext.onClick.AddListener(() =>
-            { this.NextPage(); });
-
-        this.btnPrev.onClick.AddListener(() =>
-        { this.PrevPage(); });
+        if (database.Count == 1)
+        {
+            btnNext.gameObject.SetActive(false);
+        }
+        btnPrev.gameObject.SetActive(false);
     }
     public void ExitDex()
     {
@@ -42,134 +32,47 @@ public class BtnManager : MonoBehaviour
     }
 
     public void NextPage()
-    {
-        if (this.currentPage == this.totalPage)
-        {
-            return;
-        }
-        this.currentPage++;
+    {       
+        currentPage++;
 
-        Debug.LogFormat("currentPage: {0}, totalPage: {1}", 
-            this.currentPage, this.totalPage);
+        // Debug.LogFormat("currentPage: {0}, totalPage: {1}", currentPage, this.totalPage);
 
-        if (this.currentPage == this.totalPage)
+        if (currentPage == database.Count - 1)
         {
-            this.btnNext.gameObject.SetActive(false);
+            btnNext.gameObject.SetActive(false);
         }
         else
         {
-            this.btnNext.gameObject.SetActive(true);
+            btnNext.gameObject.SetActive(true);
         }
-        this.btnPrev.gameObject.SetActive(true);
+        btnPrev.gameObject.SetActive(true);
 
-        if (currentPage == 1)
-        {
-            plantPr1.gameObject.SetActive(true);
-            evText1.gameObject.SetActive(true);
-        }
-        else
-        {
-            plantPr1.gameObject.SetActive(false);
-            evText1.gameObject.SetActive(false);
-        }
-
-        if (currentPage == 2)
-        {
-            plantPr2.gameObject.SetActive(true);
-            evText2.gameObject.SetActive(true);
-        }
-        else
-        {
-            plantPr2.gameObject.SetActive(false);
-            evText2.gameObject.SetActive(false);
-        }
-
-        if (currentPage == 3)
-        {
-            plantPr3.gameObject.SetActive(true);
-            evText3.gameObject.SetActive(true);
-        }
-        else
-        {
-            plantPr3.gameObject.SetActive(false);
-            evText3.gameObject.SetActive(false);
-        }
-
-        if (currentPage == 4)
-        {
-            plantPr4.gameObject.SetActive(true);
-            evText4.gameObject.SetActive(true);
-        }
-        else
-        {
-            plantPr4.gameObject.SetActive(false);
-            evText4.gameObject.SetActive(false);
-        }
-
+        SetNewData();
     }
     public void PrevPage()
     {
-        if (this.currentPage == 1)
-        {
-            return;
-        }
-        this.currentPage--;
+        currentPage--;
 
-        Debug.LogFormat("currentPage: {0}, totalPage: {1}",
-            this.currentPage, this.totalPage);
+        // Debug.LogFormat("currentPage: {0}, totalPage: {1}", this.currentPage, this.totalPage);
 
-        if (this.currentPage == 1)
+        if (currentPage == 0)
         {
-            this.btnPrev.gameObject.SetActive(false);
+            btnPrev.gameObject.SetActive(false);
         }
         else
         {
-            this.btnPrev.gameObject.SetActive(true);
+            btnPrev.gameObject.SetActive(true);
         }
-        this.btnNext.gameObject.SetActive(true);
+        btnNext.gameObject.SetActive(true);
 
-        if (currentPage == 1)
-        {
-            plantPr1.gameObject.SetActive(true);
-            evText1.gameObject.SetActive(true);
-        }
-        else
-        {
-            plantPr1.gameObject.SetActive(false);
-            evText1.gameObject.SetActive(false);
-        }
+        SetNewData();
+    }
 
-        if (currentPage == 2)
-        {
-            plantPr2.gameObject.SetActive(true);
-            evText2.gameObject.SetActive(true);
-        }
-        else
-        {
-            plantPr2.gameObject.SetActive(false);
-            evText2.gameObject.SetActive(false);
-        }
-
-        if (currentPage == 3)
-        {
-            plantPr3.gameObject.SetActive(true);
-            evText3.gameObject.SetActive(true);
-        }
-        else
-        {
-            plantPr3.gameObject.SetActive(false);
-            evText3.gameObject.SetActive(false);
-        }
-
-        if (currentPage == 4)
-        {
-            plantPr4.gameObject.SetActive(true);
-            evText4.gameObject.SetActive(true);
-        }
-        else
-        {
-            plantPr4.gameObject.SetActive(false);
-            evText4.gameObject.SetActive(false);
-        }
+    private void SetNewData()
+    {
+        Sprite newSprite = dexSprite.GetComponent<Sprite>();
+        newSprite = database[currentPage].itemIcon;
+        dexTitleText.text = database[currentPage].itemName;
+        dexDescText.text = database[currentPage].itemDesc;
     }
 }
