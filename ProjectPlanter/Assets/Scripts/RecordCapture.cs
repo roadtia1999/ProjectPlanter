c12f17e3c5fe00b2cd658915bb2dc9d210c7f052
roadtia1999 <132754149+roadtia1999@users.noreply.github.com>
2024-06-24 17:11:08 +0900

    RecordsScene v.1.0.a

--- ProjectPlanter/Assets/Scripts/RecordCapture.cs
+++ ProjectPlanter/Assets/Scripts/RecordCapture.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 using System.IO;
+using UnityEngine.UI;
 
 
 // 매일마다 메인 씬의 상황을 스크린샷으로 남기는 코드입니다
@@ -26,6 +27,14 @@ public class RecordCapture : MonoBehaviour
     {
         // 씬이 전부 로드될 때까지 대기
         yield return new WaitForEndOfFrame();
+
+        // 페이드 인/아웃 때문에 어둡게 스크린샷이 찍히는 경우 방지
+        Image fadeImage = GameObject.Find("FadeCanvas").GetComponentInChildren<Image>(); // 씬 전환용 이미지를 가져옴
+        Color fadeCheck = fadeImage.color;
+        while (fadeCheck.a != 0f) // 만약 씬 전환용 이미지가 완전 투명하지 않으면 대기
+        {
+            yield return null;
+        }
 
         // 현재 날짜가 지난번 기록된 날짜와 다른지 체크
         string autoCaptureDate = PlayerPrefs.GetString("AutoCaptureDate");
