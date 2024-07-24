13902c1b86a42b55ba44126e500f13db8fff8925
roadtia1999 <132754149+roadtia1999@users.noreply.github.com>
2024-07-24 14:57:13 +0900

    스크린샷 캡쳐가 정상적으로 작동하지 않는 상황 수정
    - Build 상태에서만 가동함

--- ProjectPlanter/Assets/Scripts/RecordCapture.cs
+++ ProjectPlanter/Assets/Scripts/RecordCapture.cs
@@ -34,43 +34,49 @@ public class RecordCapture : MonoBehaviour
             // 자동 스크린샷 실행하지 않음
             yield break;
         }
-
-        // 페이드 인/아웃 때문에 어둡게 스크린샷이 찍히는 경우 방지
-        Image fadeImage = GameObject.Find("FadeCanvas").GetComponentInChildren<Image>(); // 씬 전환용 이미지를 가져옴
-        Color fadeCheck = fadeImage.color;
-        while (fadeCheck.a != 0f) // 만약 씬 전환용 이미지가 완전 투명하지 않으면 대기
+        else
         {
-            yield return null;
-        }
-
-        // 현재 날짜가 지난번 기록된 날짜와 다른지 체크
-        string autoCaptureDate = PlayerPrefs.GetString("AutoCaptureDate");
-        if (autoCaptureDate != DateTime.Now.ToString("yyyy/MM/dd"))
-        {
-            // 화면의 크기를 가지는 텍스쳐를 준비
-            int width = Screen.width;
-            int height = Screen.height;
-            Texture2D tex = new Texture2D(width, height, TextureFormat.RGB24, false);
-
-            // 화면을 텍스쳐에 읽어냄
-            tex.ReadPixels(new Rect(0, 0, width, height), 0, 0);
-            tex.Apply();
-
-            // 텍스쳐를 PNG로 변환한 뒤 텍스쳐는 파괴
-            byte[] byteData = tex.EncodeToPNG();
-            Destroy(tex);
-
-            // 만약 캡쳐 파일을 저장할 폴더가 없다면 생성
-            if (!Directory.Exists(Application.dataPath + "/RecordScreenshots"))
+            // 페이드 인/아웃 때문에 어둡게 스크린샷이 찍히는 경우 방지
+            Image fadeImage = GameObject.Find("FadeCanvas").GetComponentInChildren<Image>(); // 씬 전환용 이미지를 가져옴
+            Color fadeCheck = fadeImage.color;
+            while (fadeCheck.a != 0f) // 만약 씬 전환용 이미지가 완전 투명하지 않으면 대기
             {
-                Directory.CreateDirectory(Application.dataPath + "/RecordScreenshots");
+                fadeCheck = fadeImage.color; // 체크용 변수 갱신
+                yield return null;
             }
 
-            // 변환된 PNG 파일을 어플리케이션 경로에 저장
-            File.WriteAllBytes(Application.dataPath + "/RecordScreenshots/" + DateTime.Now.ToString("yyyy/MM/dd") + ".png", byteData);
+            // 현재 날짜가 지난번 기록된 날짜와 다른지 체크
+            string autoCaptureDate = PlayerPrefs.GetString("AutoCaptureDate");
+            if (autoCaptureDate != DateTime.Now.ToString("yyyy/MM/dd"))
+            {
+                // 만약 캡쳐 파일을 저장할 폴더가 없다면 생성
+                if (!Directory.Exists(Application.dataPath + "/RecordScreenshots"))
+                {
+                    Directory.CreateDirectory(Application.dataPath + "/RecordScreenshots");
+                }
 
-            // 마지막 캡쳐 일자를 저장하는 PlayerPrefs 갱신
-            PlayerPrefs.SetString("AutoCaptureDate", DateTime.Now.ToString("yyyy/MM/dd"));
+                // 화면의 크기를 가지는 텍스쳐를 준비
+                int width = Screen.width;
+                int height = Screen.height;
+                Debug.Log(width + " " + height);
+                Texture2D tex = new Texture2D(width, height, TextureFormat.RGB24, false);
+
+                #if !UNITY_EDITOR
+                tex.ReadPixels(new Rect(0, 0, width, height), 0, 0);
+                tex.Apply();
+
+                // 텍스쳐를 PNG로 변환한 뒤 텍스쳐는 파괴
+                byte[] byteData = tex.EncodeToPNG();
+                Destroy(tex);
+
+                // 변환된 PNG 파일을 어플리케이션 경로에 저장
+                File.WriteAllBytes(Application.dataPath + "/RecordScreenshots/" + DateTime.Now.ToString("yyyy/MM/dd") + ".png", byteData);
+
+                // 마지막 캡쳐 일자를 저장하는 PlayerPrefs 갱신
+                PlayerPrefs.SetString("AutoCaptureDate", DateTime.Now.ToString("yyyy/MM/dd"));
+                #endif
+            }
         }
+        
     }
 }
